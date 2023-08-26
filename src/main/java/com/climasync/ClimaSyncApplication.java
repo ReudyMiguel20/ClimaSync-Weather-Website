f17a83f1397408ce4b9a96d150f046a83a421b8b
Reudy Guerrero <reudymiguel20@gmail.com>
2023-08-26 17:48:36 -0400

    Uploading to github

---
+++ src/main/java/com/climasync/ClimaSyncApplication.java
@@ -0,0 +1,13 @@
+package com.climasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimaSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/climasync/ClimaSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
